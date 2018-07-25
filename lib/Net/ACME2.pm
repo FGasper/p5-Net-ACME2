708e91cde1674d80428d30a2ac7d1a64f54f2e0a
Felipe Gasper <felipe@felipegasper.com>
2018-07-24 21:47:32 -0400

    trial 3 - improve LE module

--- lib/Net/ACME2.pm
+++ lib/Net/ACME2.pm
@@ -140,7 +140,7 @@ use Net::ACME2::HTTP;
 use Net::ACME2::Order;
 use Net::ACME2::Authorization;
 
-our $VERSION = '0.21-TRIAL2';
+our $VERSION = '0.21-TRIAL3';
 
 use constant {
     _JWK_THUMBPRINT_DIGEST => 'sha256',
